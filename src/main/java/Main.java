9a4ef5dffb19e43d47c6f6b6033cc67b7c446aac
0scarCollins <05410@farnborough.ac.uk>
2020-11-05 13:53:11 +0000

    test

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String differentvar = "hello world";
+        System.out.println(differentvar);
+        System.out.println(differentvar.length());
+//
+    }
+}
